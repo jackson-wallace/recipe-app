fa5574d2d7f3400b276fc75a6d6ba6ba39276b8a
Jackson Wallace <jwallace1410@gmail.com>
2024-09-10 20:32:24 -0400

    💄chore(tabs): Added custom icons and colors to tab bar

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,72 +1,68 @@
 import { Tabs } from "expo-router";
-import { View } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons/faNewspaper";
+import { faBook } from "@fortawesome/free-solid-svg-icons/faBook";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons/faTrophy";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: "#282425",
+        tabBarInactiveTintColor: "#A8A4A5",
         headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#EBE2CA",
+          borderTopColor: "#A8A4A5",
+          shadowColor: "transparent",
+        },
       }}
     >
       <Tabs.Screen
         name="feed"
         options={{
           title: "Feed",
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon
-          //     name={focused ? "home" : "home-outline"}
-          //     color={color}
-          //   />
-          // ),
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faNewspaper} color={color} size={20} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="lists"
+        name="books"
         options={{
-          title: "Lists",
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon
-          //     name={focused ? "code-slash" : "code-slash-outline"}
-          //     color={color}
-          //   />
-          // ),
+          title: "Your Books",
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faBook} color={color} size={20} />
+          ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: "Search",
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon
-          //     name={focused ? "code-slash" : "code-slash-outline"}
-          //     color={color}
-          //   />
-          // ),
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faMagnifyingGlass} color={color} size={20} />
+          ),
         }}
       />
       <Tabs.Screen
         name="leaderboard"
         options={{
           title: "Leaderboard",
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon
-          //     name={focused ? "code-slash" : "code-slash-outline"}
-          //     color={color}
-          //   />
-          // ),
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faTrophy} color={color} size={20} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon
-          //     name={focused ? "code-slash" : "code-slash-outline"}
-          //     color={color}
-          //   />
-          // ),
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faUser} color={color} size={20} />
+          ),
         }}
       />
     </Tabs>
